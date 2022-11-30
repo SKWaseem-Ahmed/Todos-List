60b71df0dd37c876e027dfe5b71f02c67952476b
SKWaseem-Ahmed <shaikwaseemahmed03@gmail.com>
2022-11-30 19:12:01 +0530

    Initial Commit

--- src/MyComponents/AddTodo.js
+++ src/MyComponents/AddTodo.js
@@ -21,23 +21,23 @@ export const AddTodo = ({addTodo}) => {
 
 
     return (
-        <div  className="container text-center my-3">
+        <div className="container text-center my-3">
             <h3><strong>Add a Todo</strong></h3>
             <form onSubmit={submit}>
                 <div className="mb-3" >
                     <label htmlFor="title" className="form-label"><strong>Todo Title</strong></label>
-                    <input type="text"  style={{
-                    border:"2px solid #666"
-                }} value={title} onChange = {(e)=>{setTitle(e.target.value)}} className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"/>
+                    <input type="text" style={{
+                        border: "2px solid #666"
+                    }} value={title} onChange={(e) => { setTitle(e.target.value) }} className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
                 </div>
                 <div className="mb-3">
                     <label htmlFor="desc" className="form-label"><strong>Todo Description</strong></label>
-                    <input type="text"  style={{
-                    border:"2px solid #666"
-                }} value={desc} onChange = {(e)=>{setDesc(e.target.value)}} className="form-control" id="exampleInputPassword1"/>
+                    <input type="text" style={{
+                        border: "2px solid #666"
+                    }} value={desc} onChange={(e) => { setDesc(e.target.value) }} className="form-control" id="exampleInputPassword1" />
                 </div>
-                    <button type="submit" className="btn btn-sm btn-success">Add Todo</button>
+                <button type="submit" className="btn btn-sm btn-success">Add Todo</button>
             </form>
         </div>
-                    )
+    );
 }
